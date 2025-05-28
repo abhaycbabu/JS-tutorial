523bd24c86c9fb80bd282b32376f3ed1e046a749
abhaycbabu <abhaycbabu@codeace.com>
2025-05-28 14:15:24 +0530

    Add the 2 files

---
+++ sample.js
@@ -0,0 +1,9 @@
+var a = document.getElementsByClassName("head1");
+var b = document.getElementsByName("text")
+console.log(a);
+function showMessage() {
+      alert("Hello from JavaScript!");
+      b.Inn
+    }
+
+var b 
